a57c06e7f6dcd0fd736cc7129162f877ded23f53
shuoyanhe <yanyanho@126.com>
2019-09-03 18:58:08 +0800

    delete the useless code

--- src/main/java/com/webank/webase/front/trade/trade/HTLCInfoRepository.java
+++ src/main/java/com/webank/webase/front/trade/trade/HTLCInfoRepository.java
@@ -1,15 +1,8 @@
 package com.webank.webase.front.trade.trade;
 
 
-import com.webank.webase.front.keystore.KeyStoreInfo;
-import com.webank.webase.front.monitor.Monitor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.util.List;
 
 @Repository
 public interface HTLCInfoRepository extends CrudRepository<HTLCInfo, Long> {
